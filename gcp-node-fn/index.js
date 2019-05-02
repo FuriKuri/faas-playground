4342ce4a0865d2a2b3c1c2c52cd87ccc8dc4ba92
Theo Pack <tf.pack@gmail.com>
2019-05-02 08:50:19 +0200

    Add CORS headers

--- gcp-node-fn/index.js
+++ gcp-node-fn/index.js
@@ -1,6 +1,6 @@
 exports.helloWorld = (req, res) => {
   let name = req.query.name || req.body.name || 'World';
-  res.set('Access-Control-Allow-Origin', 'furikuri.net');
+  res.set('Access-Control-Allow-Origin', 'https://furikuri.net');
   res.set('Access-Control-Allow-Methods', 'GET, POST');
   res.set('Access-Control-Allow-Headers', 'Content-Type');
   res.status(200).send('Hello ' + name + '! From GCP + Node');
