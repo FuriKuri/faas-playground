bab3625333de20bf885d387b488ed19e7d055042
Theo Pack <tf.pack@gmail.com>
2019-05-02 08:50:29 +0200

    Add CORS headers

--- gcp-go-fn/function.go
+++ gcp-go-fn/function.go
@@ -6,6 +6,13 @@ import (
 	"html"
 	"net/http"
 )
+
+func enableCors(w *http.ResponseWriter) {
+    (*w).Header().Set("Access-Control-Allow-Origin", "https://furikuri.net")
+    (*w).Header().Set("Access-Control-Allow-Methods", "GET, POST")
+    (*w).Header().Set("Access-Control-Allow-Headers", "Content-Type")
+}
+
 func HelloWorld(w http.ResponseWriter, r *http.Request) {
 	var d struct {
 		Name string `json:"name"`
@@ -16,5 +23,6 @@ func HelloWorld(w http.ResponseWriter, r *http.Request) {
 	if d.Name != "" {
 		name = d.Name
 	}
+    enableCors(&w)
 	fmt.Fprint(w, html.EscapeString("Hello " + name + "! From GCP + Go"))
 }
